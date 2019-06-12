61b02a8d07d18c24e105ac664eca029564d74bed
fxa-devs <fxa-core@mozilla.com>
2019-06-12 16:17:22 +0000

    chore(docs): rebuild docs [skip ci]

---
+++ fxa-payments-server/precache-manifest.60c53b0cd5a333570ba529118f621785.js
@@ -0,0 +1,37 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "3.91af501dbdeeb8857cec.bundle.js"
+  },
+  {
+    "url": "4.91af501dbdeeb8857cec.bundle.js"
+  },
+  {
+    "revision": "1911428efab9636bf6f0161379655e0e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.91af501dbdeeb8857cec.bundle.js"
+  },
+  {
+    "revision": "a3c1d9c9ebc9009155a9",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.91af501dbdeeb8857cec.bundle.js"
+  },
+  {
+    "revision": "16821f55a71babf1bab599685a91f542",
+    "url": "static/media/firefox-logo.16821f55.svg"
+  },
+  {
+    "revision": "0cb1bdb24a29fae2a1b07a552f985328",
+    "url": "static/media/lock.0cb1bdb2.svg"
+  },
+  {
+    "revision": "c86df1d37d3bba2d62c4a40881cca7b6",
+    "url": "static/media/mozilla.c86df1d3.svg"
+  },
+  {
+    "url": "vendors~main.91af501dbdeeb8857cec.bundle.js"
+  }
+]);
