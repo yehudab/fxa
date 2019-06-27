5e0b178db9cc363a8e4147aeb383658c263571d3
fxa-devs <fxa-core@mozilla.com>
2019-06-27 10:55:43 +0000

    chore(docs): rebuild docs [skip ci]

---
+++ fxa-email-service/implementors/rocket/data/from_data/trait.FromDataSimple.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fxa_email_service"] = [{text:"impl <a class=\"trait\" href=\"https://api.rocket.rs/v0.4/rocket/data/from_data/trait.FromDataSimple.html\" title=\"trait rocket::data::from_data::FromDataSimple\">FromDataSimple</a> for <a class=\"struct\" href=\"fxa_email_service/api/send/struct.Email.html\" title=\"struct fxa_email_service::api::send::Email\">Email</a>",synthetic:false,types:["fxa_email_service::api::send::Email"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
